4944dbbd278f027a7ce7193db757cfd687da6c76
Yasín Muñoz <Yasin.Munoz@alu.uclm.es>
2018-09-25 16:38:33 +0200

    Creacion Base de Datos

--- StartGrow/src/StartGrow/Data/Migrations/20180925143704_CreateDatabase.Designer.cs
+++ StartGrow/src/StartGrow/Data/Migrations/20180925143704_CreateDatabase.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace StartGrow.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180925143704_CreateDatabase")]
+    partial class CreateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
