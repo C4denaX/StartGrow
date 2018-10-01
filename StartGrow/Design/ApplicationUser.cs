bad6cd4812de4822448f5b72b2fdf810ef780e95
David Girón López <David.Giron@alu.uclm.es>
2018-10-01 19:49:01 +0200

    Creado el diagrama de clases RecuperarInversion

---
+++ StartGrow/Design/ApplicationUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace StartGrow.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public virtual string Nombre { get; set; }
+        [Required]
+        public virtual string Apellido1 { get; set; }
+        [Required]
+        public virtual string Apellido2 { get; set; }
+        [Required]
+        public virtual string Nif { get; set; }
+        [Required]
+        public virtual string Nacionalidad { get; set; }
+        [Required]
+        public virtual string PaisResidencia { get; set; }
+        [Required]
+        public virtual string Provincia { get; set; }
+        [Required]
+        public virtual string Municipio { get; set; }
+        [Required]
+        public virtual string Domicilio { get; set; }
+        [Required]
+        public virtual int CodPost { get; set; }
+    }
+
+}
---
+++ StartGrow/Design/Monedero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartGrow.Models
+{
+    public class Monedero
+    {
+        [Key]
+        public virtual int MonederoId { get; set; }
+
+        [Required]
+        public virtual int InversorId { get; set; }
+        [ForeignKey("InversorId")]
+        public virtual Inversor Inversor { get; set; }
+
+        [Required]
+        public virtual float Dinero { get; set; }
+    }
+}
