5c7043e0749dd5420e30a652edc62c4f0e8bc963
Yasín Muñoz <Yasin.Munoz@alu.uclm.es>
2018-10-02 16:21:56 +0200

    Errores de Compilación Resueltos

--- StartGrow/Design/ApplicationUser.cs
+++ StartGrow/Design/ApplicationUser.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+
+
 
 namespace StartGrow.Models
 {
