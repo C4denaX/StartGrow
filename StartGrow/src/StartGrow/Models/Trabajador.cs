181fca3a32e606d02cf15975ece88dabbbe1e39b
Yasín Muñoz <Yasin.Munoz@alu.uclm.es>
2018-10-02 11:50:14 +0200

    Modificada la Clase Trabajador

--- StartGrow/src/StartGrow/Models/Trabajador.cs
+++ StartGrow/src/StartGrow/Models/Trabajador.cs
@@ -8,8 +8,7 @@ namespace StartGrow.Models
 {
     public class Trabajador : ApplicationUser
     {
-        [Key]
-        public virtual int TrabajadorID { get; set; }
+        
         [Required]
         public virtual string PuestoDeTrabajo { get; set; }
 
