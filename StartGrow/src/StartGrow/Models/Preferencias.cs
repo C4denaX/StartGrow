4d7f3603435889ebbc8320a50589437e2437d7b2
David Girón López <David.Giron@alu.uclm.es>
2018-10-08 20:24:18 +0200

    Editada la clase Preferencias

--- StartGrow/src/StartGrow/Models/Preferencias.cs
+++ StartGrow/src/StartGrow/Models/Preferencias.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,70 @@ namespace StartGrow.Models
 {
     public class Preferencias
     {
+        [Key]
+        public virtual int ID
+        {
+            get;
+            set;
+        }
+        [ForeignKey("TiposInversionesID")]
+        public string TiposInversionesID
+        {
+            get;
+            set;
+        }
+        public virtual TiposInversiones TiposInversiones
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("RatingID")]
+        public string RatingID
+        {
+            get;
+            set;
+        }
+        public virtual Rating Rating
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("AreasID")]
+        public string AreasID
+        {
+            get;
+            set;
+        }
+        public virtual Areas Areas
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("ApplicationUserId")]
+        public int ApplicationUserId
+        {
+            get;
+            set;
+        }
+        public virtual ApplicationUser ApplicationUser
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("ProyectoId")]
+        public int ProyectoId
+        {
+            get;
+            set;
+        }
+        public virtual Proyecto Proyecto
+        {
+            get;
+            set;
+        }
     }
 }
