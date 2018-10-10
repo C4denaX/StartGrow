9b3a8b73c6d80b9640d3d46c4cb9b595bcb0d388
Yasín Muñoz <Yasin.Munoz@alu.uclm.es>
2018-10-10 20:59:23 +0200

    Agregada y Definida la Clase ProyectoAreas

---
+++ StartGrow/Design/ProyectoAreas.cs
@@ -0,0 +1,47 @@
+﻿using StartGrow.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Design
+{
+    public class ProyectoAreas
+    {
+
+        [Key]
+        public virtual int ID
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [ForeignKey("ProyectoId")]
+        public virtual Proyecto Proyecto
+        {
+            get;
+            set;
+        }
+        public virtual int ProyectoId
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [ForeignKey("AreasId")]
+        public virtual Areas Areas
+        {
+            get;
+            set;
+        }
+        public virtual string AreasId
+        {
+            get;
+            set;
+        }
+    }
+}
