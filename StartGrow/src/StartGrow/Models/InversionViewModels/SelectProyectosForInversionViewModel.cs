f1e934c361a5b6777f7ca4b35da96ae18c1337ab
Yasín Muñoz <Yasin.Munoz@alu.uclm.es>
2018-10-23 01:30:40 +0200

    Creado GET y POST
    Elementos de trabajo relacionados: #6

---
+++ StartGrow/src/StartGrow/Models/InversionViewModels/SelectProyectosForInversionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartGrow.Models.InversionViewModels
+{
+    public class SelectProyectosForInversionViewModel
+    {
+        //Lista de Proyectos
+        public IEnumerable <Proyecto> Proyectos { get; set; }
+
+        //Utilizado para filtrar por Areas
+        [Display(Name = "Areas")]
+        public SelectList Areas;
+        
+        //Utilizado para filtrar por Rating
+        [Display(Name = "Rating")]
+        public SelectList Rating;
+
+        //Utilizado para filtrar por TiposInversiones
+        [Display(Name = "TiposInversiones")]
+        public SelectList TiposInversiones;
+        
+
+        public IList<int> IdsToAdd { get; set; }
+    }
+}
