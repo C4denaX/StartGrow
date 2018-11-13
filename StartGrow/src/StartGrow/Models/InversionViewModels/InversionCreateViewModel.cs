0a6c355e654c541990261a723bd41e237b2df35b
Yasín Muñoz <Yasin.Munoz@alu.uclm.es>
2018-11-13 01:01:38 +0100

    Creado InversionCreateViewModel
    Elementos de trabajo relacionados: #10

---
+++ StartGrow/src/StartGrow/Models/InversionViewModels/InversionCreateViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartGrow.Models.InversionViewModels
+{
+    public class InversionCreateViewModel
+    {
+        public InversionCreateViewModel()
+        {
+            Inversiones= new List<Inversion>();
+        }
+
+        public virtual string Name
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Primer Apellido")]
+        public virtual string FirstSurname
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Segundo Apellido")]
+        public virtual string SecondSurname
+        {
+            get;
+            set;
+        }
+
+        //It will be necessary whenever we need a relationship with ApplicationUser or any child class
+        public string InversorId
+        {
+            get;
+            set;
+        }
+        [Required]
+        public virtual float Cuota
+        {
+            get;
+            set;
+        }
+        [Required]
+        public virtual float Intereses
+        {
+            get;
+            set;
+        }        
+        public virtual string EstadosInversiones
+        {
+            get;
+            set;
+        }        
+        public virtual float Total
+        {
+            get;
+            set;
+        }
+        public virtual IList<Inversion> Inversiones
+        {
+            get;
+            set;
+        }        
+    }
+}
