32ffe500571c3202cbaa9cbc4aeeb5d31f26af56
DAVID GIRÓN LÓPEZ <David.Giron@alu.uclm.es>
2018-11-27 10:12:57 +0100

    Migraciones creadas para modificar el modelo de Monedero
    Elementos de trabajo relacionados: #14

--- StartGrow/src/StartGrow/Migrations/20181120185421_MigracionMonederoActualizado.Designer.cs
+++ StartGrow/src/StartGrow/Migrations/20181120185421_MigracionMonederoActualizado.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace StartGrow.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181120185421_MigracionMonederoActualizado")]
+    partial class MigracionMonederoActualizado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
